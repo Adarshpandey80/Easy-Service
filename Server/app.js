fe9cfe1ffd07fbb5f56808b557ce9b01efdf3c78
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-08-31 23:49:20 +0530

    new route create

--- Server/app.js
+++ Server/app.js
@@ -8,6 +8,10 @@ app.get("/", (req,res)=>{
     res.send("Hello World");
 })
 
+app.get("/about", (req,res)=>{
+    res.send("About Page");
+})
+
 app.listen(3004, ( ) => {
     console.log('Server is running on port 3004');
 })
